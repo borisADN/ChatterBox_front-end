fd8681f5543bef04582047245638c40e856d58d8
= <edemwomagno@gmail.com>
2024-10-02 10:10:42 +0000

    mercredi le 2/08/24

--- src/Pages/Group/Group.jsx
+++ src/Pages/Group/Group.jsx
@@ -11,102 +11,115 @@ export default function GroupChat() {
     const [groups, setGroups] = useState([]);
     const [selectedGroup, setSelectedGroup] = useState(null);
     const [messages, setMessages] = useState([]);
+    const [memberCount, setMemberCount] = useState(null);
     const [message, setMessage] = useState("");
     const [search, setSearch] = useState("");
     const chatRef = useRef(null);
     const [selectedFile, setSelectedFile] = useState(null);
-    const defaultAvatar = 'http://localhost:8000/uploads/default-avatar.png';
+    
+    // Référence audio pour le son des messages reçus
+    const audioRef = useRef(new Audio('/audio/message-received.mp3'));
+    const [lastMessageId, setLastMessageId] = useState(null); // ID du dernier message
+
     const formatDate = (dateString) => {
         const date = new Date(dateString);
-        return format(date, 'dd/MM/yyyy HH:mm'); // Format souhaité : jour/mois/année heures:minutes
+        if (isNaN(date.getTime())) {
+            console.error('Invalid date:', dateString);
+            return 'Date invalide';
+        }
+        return format(date, 'dd/MM/yyyy HH:mm');
     };
 
-    // Appel à l'API pour récupérer les groupes de l'utilisateur connecté
     useEffect(() => {
         const fetchGroups = async () => {
-            const userId = localStorage.getItem("UserId"); // ID de l'utilisateur connecté
-
+            const userId = localStorage.getItem("UserId");
             try {
                 const response = await axios.post('http://localhost:8000/api/SelectGroups', { member_id: userId });
-                // console.log(response.data.groups.avatar);
-                // console.log(groups);
-                
-                
-                setGroups(response.data.groups); // Mettre à jour les groupes récupérés
+                setGroups(response.data.groups); 
             } catch (error) {
                 console.error("Erreur lors de la récupération des groupes:", error);
             }
         };
-
         fetchGroups();
-    }, []); // Le useEffect est exécuté une seule fois au chargement du composant
+    }, []);
 
     const handleGroupSelect = async (group) => {
         setSelectedGroup(group);
-        setMessages([]); // Réinitialiser les messages lorsqu'un groupe est sélectionné
+        setMessages([]);
+        await fetchMessages(group.id); // Charger les messages dès qu'un groupe est sélectionné
+    };
 
+    const fetchMessages = async (groupId) => {
         try {
-            const response = await axios.post('http://localhost:8000/api/getGroupMessages', { group_id: group.id });
-            // console.log(messages.sender_id);
-            setMessages(response.data.messages); // Mettre à jour les messages récupérés pour ce groupe
+            const response = await axios.post('http://localhost:8000/api/getGroupMessages', { group_id: groupId });
+            setMessages(response.data.messages);
         } catch (error) {
             console.error("Erreur lors de la récupération des messages du groupe:", error);
         }
     };
 
-    const handleSendMessage = async() => {
+    useEffect(() => {
+        let intervalId; // Variable pour stocker l'ID de l'intervalle
+        if (selectedGroup) {
+            fetchMessages(selectedGroup.id); // Charger les messages initialement
+            intervalId = setInterval(() => {
+                fetchMessages(selectedGroup.id); // Mettre à jour les messages toutes les 4 secondes
+            }, 4000);
+        }
+
+        // Nettoyer l'intervalle lorsque le groupe sélectionné change ou le composant se démonte
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, [selectedGroup]); // Dépendance au groupe sélectionné
+
+    const handleSendMessage = async () => {
         if (!message.trim()) return;
         const newMessage = {
             group_id: selectedGroup.id,
             sender_id: localStorage.getItem("UserId"),
             message,
-            
         };
 
         await axios.post('http://localhost:8000/api/SendMessageGroup', newMessage);
-
         setMessages(prevMessages => [...prevMessages, newMessage]);
-        setMessage(""); // Réinitialiser le champ de message
-
-        // Scroll to bottom immediately after sending a message
+        setMessage("");
         scrollToBottom(); 
-
-        // Simuler une réponse d'autres membres après un délai
-    
     };
-
- 
 
     const handleSendFile = async () => {
-        if (!selectedFile) return; // Ensure a file is selected
+        if (!selectedFile) return;
 
-    const formData = new FormData();
-    formData.append("group_id", selectedGroup.id);
-    formData.append("sender_id", localStorage.getItem("UserId"));
-    formData.append("message", "Fichier partagé"); // Set default message
-    formData.append("file", selectedFile); // Append the selected file
+        const formData = new FormData();
+        formData.append("group_id", selectedGroup.id);
+        formData.append("sender_id", localStorage.getItem("UserId"));
+        formData.append("message", "Fichier partagé");
+        formData.append("file", selectedFile);
 
-    try {
-        const response = await axios.post('http://localhost:8000/api/SendMessageGroup', formData, {
-            headers: {
-                'Content-Type': 'multipart/form-data', // Set content type for file upload
-            },
-        });
-
-        // Update messages state with the new message and file data
-        setMessages(prevMessages => [...prevMessages, response.data.data]); // Use the response data directly
-        setSelectedFile(null); // Reset the selected file
-
-        scrollToBottom(); // Scroll to the bottom after sending
-    } catch (error) {
-        console.log(error.message);
-        
-        console.error("Erreur lors de l'envoi du fichier:", error);
-    } 
+        try {
+            const response = await axios.post('http://localhost:8000/api/SendMessageGroup', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+            setMessages(prevMessages => [...prevMessages, response.data.data]);
+            setSelectedFile(null);
+            scrollToBottom(); 
+        } catch (error) {
+            console.error("Erreur lors de l'envoi du fichier:", error);
+        } 
     };
-    
 
-
+    const MemberCount = async () => {
+        try {
+            const response = await axios.post('http://localhost:8000/api/countMembers', { 
+                group_id: selectedGroup.id 
+            });
+            setMemberCount(response.data.count);
+        } catch (error) {
+            console.error("Erreur lors de la récupération du nombre de membres du groupe:", error);
+        }
+    };
 
     const scrollToBottom = () => {
         if (chatRef.current) {
@@ -117,6 +130,27 @@ export default function GroupChat() {
     const filteredGroups = groups.filter(group => 
         group.name.toLowerCase().includes(search.toLowerCase())
     );
+
+    useEffect(() => {
+        if (selectedGroup) {
+            MemberCount();
+        }
+    }, [selectedGroup]);
+
+    // Surveiller les nouveaux messages et jouer le son
+    useEffect(() => {
+        // Vérifie si le tableau des messages n'est pas vide avant d'accéder au dernier message
+        if (messages.length > 0) {
+            const lastMessageIndex = messages.length - 1;
+            const lastMessage = messages[lastMessageIndex];
+
+            // Jouer le son uniquement si le dernier message est différent et provient d'un autre utilisateur
+            if (lastMessage.id !== lastMessageId && lastMessage.sender_id !== localStorage.getItem("UserId")) {
+                audioRef.current.play();
+                setLastMessageId(lastMessage.id); // Mettre à jour l'ID du dernier message
+            }
+        }
+    }, [messages]); // Exécuter cet effet lorsqu'il y a des changements dans les messages
 
     return (
         <div style={UserStyle.Container} className="Poppins">
@@ -138,18 +172,15 @@ export default function GroupChat() {
                         {filteredGroups.map(group => (
                             <div 
                                 key={group.id} 
-                                className={`contact ${selectedGroup?.id === group.id ? 'active' : ''}`} // Add active class if selected
+                                className={`contact ${selectedGroup?.id === group.id ? 'active' : ''}`} 
                                 onClick={() => handleGroupSelect(group)} 
                                 style={{ cursor: "pointer", padding: '10px', borderBottom: '1px solid #ccc' }}
                             >
-                                
                                 <div className="pic" 
                                      style={{ 
                                          width: '40px', 
                                          height: '40px', 
-                                        //  backgroundImage: `url(${group.avatar})`, 
                                          backgroundImage: `url(http://localhost:8000/uploads/${group.avatar})`,
-
                                          backgroundSize: 'cover', 
                                          borderRadius: '50%', 
                                          marginRight: '10px' 
@@ -161,7 +192,7 @@ export default function GroupChat() {
                     </div>
                     <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '10px' }}>
                         <a href="/chat">retour</a>
-                        <a href="/createGroup">Creer</a>
+                        <a href="/createGroup" title="Creer un nouveau groupe">Creer</a>
                     </div>
                 </div>
 
@@ -170,90 +201,74 @@ export default function GroupChat() {
                     {selectedGroup ? (
                         <>
                             <div className="bar" style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
-                            <a href={`/GroupDetails/${selectedGroup.id}`}>
-                                <div className="group-pic" 
-                                     style={{ 
-                                         width: '40px', 
-                                         height: '40px', 
-                                         backgroundImage: `url(http://localhost:8000/uploads/${selectedGroup.avatar })`, 
-                                         backgroundSize: 'cover', 
-                                         borderRadius: '50%', 
-                                         marginRight: '10px' 
-                                     }} 
-                                /> 
+                                <a href={`/GroupDetails/${selectedGroup.id}`}>
+                                    <div className="group-pic" 
+                                         style={{ 
+                                             width: '40px', 
+                                             height: '40px', 
+                                             backgroundImage: `url(http://localhost:8000/uploads/${selectedGroup.avatar })`, 
+                                             backgroundSize: 'cover', 
+                                             borderRadius: '50%', 
+                                             marginRight: '10px' 
+                                         }} 
+                                    /> 
                                 </a>
                                 <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '100%' }}>
                                     <div className="name" style={{ fontWeight: 'bold', marginRight: '10px' }}>{selectedGroup.name}</div>
                                     <a href={`/addMember/${selectedGroup.id}`}>
-                                     <FaCirclePlus size={40} />
-
-                                     </a>
+                                        <FaCirclePlus size={40} />
+                                    </a>
                                 </div>
                             </div>
-                            {/* <h6>{selectedGroup.members.length} Membres</h6> */}
+                            
+                            <h6>{memberCount} membres</h6>
 
                             {/* Messages window */}
                             <div id="chat" ref={chatRef} style={{ overflowY: 'auto', maxHeight: '400px' }}>
-    {messages.map((msg, index) => (
-        <div
-        key={index}
-            className={`message ${msg.sender_id != localStorage.getItem("UserId") ? "" : "sender"}` } 
-            style={{
-                display: 'flex', flexDirection: 'column'
-                // justifyContent: msg.sender_id === localStorage.getItem("UserId") ? 'flex-end' : 'flex-start'
-            }}
-            // style={{
-            //     display: 'flex',
-            //     alignItems: 'center',
-            //     margin: '5px 0',
-            //     justifyContent: msg.sender_id === localStorage.getItem("UserId") ? 'flex-end' : 'flex-start'
-            // }}
-        >
-
-
-              {/* Affichage du nom de l'expéditeur */}
-              <span style={{ fontWeight: 'bold' }}>Boris</span>
-                {/* Affichage de la date d'envoi */}
-            {/* Affichage des fichiers avec icônes et prévisualisation */}
-                {/* <span style={{ fontSize: '0.8em', color: msg.sender_id === localStorage.getItem("UserId") ? '#555' : '' }}>{formatDate(msg.created_at)}</span> */}
-            {msg.file ? (
-                <div className="file-message" style={{ display: 'flex', alignItems: 'center', border: '1px solid #ccc', borderRadius: '10px'}}>
-                    {/* Si le fichier est une image */}
-                    {msg.file.match(/\.(jpg|jpeg|png|gif)$/i) ? (
-                        <div style={{ marginRight: '10px' }}>
-                            <img 
-                                src={`http://localhost:8000/uploads/sendGroupFile/${msg.file}`} target="_blank" 
-                                alt="preview" 
-                                style={{ width: '50px', height: '50px', objectFit: 'cover', borderRadius: '5px' }} 
-                            />
-                        </div>
-                    ) : (
-                        /* Si c'est un autre type de fichier (PDF, doc, etc.) */
-                        <div style={{ marginRight: '10px' }}>
-                            <FaPaperclip size={30} style={{ color: '#555' }} />
-                        </div>
-                    )}
-                    {/* Lien de téléchargement */}
-                    <div>
-                        <a 
-                            href={`http://localhost:8000/uploads/sendGroupFile/${msg.file}`} target="_blank" 
-                            download={msg.file}
-                            style={{ textDecoration: 'none', color: 'blue', fontWeight: 'bold' }}
-                        >
-                            {msg.file}
-                        </a>
-                    </div>
-                </div>
-            ) : (
-                // Affichage des messages textuels
-                <p style={{ background: msg.sender_id === localStorage.getItem("UserId") ? '#e1ffc' : '' , borderRadius: '10px' }}>
-                    {msg.message}
-                </p>
-            )}
-        </div>
-    ))}
-</div>
-
+                                {messages.map((msg, index) => (
+                                    <div
+                                        key={index}
+                                        className={`message ${msg.sender_id != localStorage.getItem("UserId") ? "" : "sender"}` } 
+                                        style={{
+                                            display: 'flex', flexDirection: 'column'
+                                        }}
+                                    >
+                                        {msg.file ? (
+                                            <div className="file-message" style={{ display: 'flex', alignItems: 'center', border: '1px solid #ccc', borderRadius: '10px'}}>
+                                                {msg.file.match(/\.(jpg|jpeg|png|gif)$/i) ? (
+                                                    <div style={{ marginRight: '10px' }}>
+                                                        <img 
+                                                            src={`http://localhost:8000/uploads/sendGroupFile/${msg.file}`} target="_blank" 
+                                                            alt="preview" 
+                                                            style={{ width: '50px', height: '50px', objectFit: 'cover', borderRadius: '5px' }} 
+                                                        />
+                                                    </div>
+                                                ) : (
+                                                    <div style={{ marginRight: '10px' }}>
+                                                        <FaPaperclip size={30} style={{ color: '#555' }} />
+                                                    </div>
+                                                )}
+                                                <div>
+                                                    <a 
+                                                        href={`http://localhost:8000/uploads/sendGroupFile/${msg.file}`} target="_blank" 
+                                                        download={msg.file}
+                                                        style={{ textDecoration: 'none', color: 'blue', fontWeight: 'bold' }}
+                                                    >
+                                                        {msg.file}
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        ) : (
+                                            <p style={{ background: msg.sender_id === localStorage.getItem("UserId") ? '#e1ffc' : '' , borderRadius: '10px' }} >
+                                                {msg.message}
+                                            </p>
+                                        )}
+                                        <span style={{ fontSize: '0.8em', color: '#555' }} >
+                                            {formatDate(msg.created_at)}
+                                        </span>
+                                    </div>
+                                ))}
+                            </div>
 
                             {/* Input and send button */}
                             <div className="input">
--- src/Pages/GroupDetails/GroupDetails.jsx
+++ src/Pages/GroupDetails/GroupDetails.jsx
@@ -1,12 +1,59 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./Style.css";
 import axios from "axios";
 
 export default function GroupDetails() {
   const { groupId } = useParams();
+  const [group, setGroup] = useState(null); // État pour les détails du groupe
+  const [members, setMembers] = useState([]); // État pour la liste des membres
   const [email, setEmail] = useState('');
   const AdderId = localStorage.getItem('UserId');
+  const [adminName, setAdminName] = useState(""); 
+useEffect(() => {
+  const fetchGroupDetails = async () => {
+    try {
+      const response = await axios.post('http://localhost:8000/api/ListGroups', {
+        group_id: groupId,
+      })
+      console.log(response.data);
+      setGroup(response.data);
+      setMembers(response.data.members);
+      const admin = response.data.members.find(member => member.id == AdderId);
+      if (admin) {
+        setAdminName(admin.name);
+      }
+      console.log(admin.name);
+      
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  fetchGroupDetails();
+  
+}, [groupId]);
+
+// const handleDelete = async (memberId) => {
+//   try {
+//     // Logique pour expulser le membre
+//     const response = await axios.post('http://localhost:8000/api/expelMember', {
+//       group_id: groupId,
+//       member_id: memberId,
+//     });
+
+//     if (response.data.success) {
+//       // Met à jour l'état des membres après l'expulsion
+//       setMembers(prevMembers => prevMembers.filter(member => member.id !== memberId));
+//       console.log("Membre expulsé avec succès");
+//     } else {
+//       console.error("Erreur lors de l'expulsion du membre");
+//     }
+//   } catch (error) {
+//     console.error("Erreur lors de l'appel à l'API:", error);
+//   }
+// };
+
+
   const handleSubmit = async(e) => {
     e.preventDefault();
     try {
@@ -40,16 +87,24 @@ export default function GroupDetails() {
       {/* Display group details here */}
       {/* Add any necessary components or styling */}
       {/* Example: <h1>Group Details</h1> */}
-      <div>
-        <img
-          style={{ width: "200px", height: "200px", borderRadius: "50%" }}
-          src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
-          alt=""
-        />
-      </div>
-      <h1>Nom Du Groupe</h1>
-      <h3>Cree par Boris le 10/05/2023</h3>
-      <h3>Membres : 2</h3>
+      {group && (
+        <>
+          <div>
+            <img
+              style={{ width: "200px", height: "200px", borderRadius: "50%" }}
+              src={`http://localhost:8000/uploads/${group.group.avatar}`}
+              alt="Group Avatar"
+            />
+          </div>
+          {/* {console.log(group.group.name)}; */}
+          
+          <h1>{group.group.name}</h1>
+          <h3>Créé par <span style={{color:"green",textDecoration:"underline"}}>{adminName}</span>  le {new Date(group.group.created_at).toLocaleDateString()}</h3>
+          <h3>Membres : {members.length}</h3>
+        </>
+      )}
+      {/* <h1>{group.name}</h1> */}
+   
       <div className="table">
         <table>
           <thead>
@@ -61,30 +116,29 @@ export default function GroupDetails() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>
-                <img
-                  style={{ width: "50px", height: "50px", borderRadius: "50%" }}
-                  src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
-                  alt=""
-                />
-              </td>
-              <td>Boris</td>
-              <td>boris@example.com</td>
-              <td><a href="">supprimer</a></td>
-            </tr>
-            <tr>
-              <td>
-                <img
-                  style={{ width: "50px", height: "50px", borderRadius: "50%" }}
-                  src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava2-bg.webp"
-                  alt=""
-                />
-              </td>
-              <td>Marie</td>
-              <td>marie@example.com</td>
-              <td><a href="">supprimer</a></td>
-            </tr>
+          {members.map((member) => (
+              <tr key={member.id}>
+                <td>
+                  <img
+                    style={{ width: "50px", height: "50px", borderRadius: "50%" }}
+                    src={`http://localhost:8000/uploads/${member.avatar}`} // Avatar du membre
+                    alt={member.name}
+                  />
+                </td>
+                <td>{member.name}</td>
+                <td>{member.email}</td>
+                <td>
+                {member.id != AdderId ? (
+                  //on peut definir cette fonction apres au besoin
+                  //et aussi tres important tout le monde ne peut pas expulser. seul l'admin peut expulser
+                    <a href="#" onClick={() => handleDelete(member.id)}>Expulser</a>
+                  ) : (
+                    <p style={{color:"red", fontWeight:"bold",fontSize:"40px"}}>ADMIN</p>
+                  )
+                  }
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
@@ -108,3 +162,4 @@ export default function GroupDetails() {
     </div>
   );
 }
+
